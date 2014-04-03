f500fef203e9ce3789a0b94be59d19f6158ecd4a
Lily <hanwen98@gmail.com>
2014-04-03 19:43:30 -0400

    asked the user how many dice they want to roll.

--- dice.py
+++ dice.py
@@ -1,3 +1,6 @@
 import random
-dice = random.randint(1,7)
+import specialInputModule
+dice = random.randint(1,6)
+side = specialInputModule.int_input("How many sides should the die have?")
+dice_number = specialInputModule.int_input("How many dice do you want to roll?")
 print "The result is %d" %(dice)
