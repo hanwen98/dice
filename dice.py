b1efcc9353a4cc4e0215809c4848f6dcc7169327
Lily <hanwen98@gmail.com>
2014-04-03 20:10:34 -0400

    wrote the pseudo code to ask users how many side they want for each die

--- dice.py
+++ dice.py
@@ -1,11 +1,11 @@
 import random
 import specialInputModule
 
-side = specialInputModule.int_input("How many sides should the die have?")
 dice_number = specialInputModule.int_input("How many dice do you want to roll?")
 
 for time in range(1,dice_number+1):
-	dice = random.randint(0,side)
+	side = specialInputModule.int_input("How many sides should the die have?")
+	dice = random.randint(1,side+1)
 	print "The result is %d" %(dice)
 
 # 
