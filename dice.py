9135a67be2428aa28566f87f1144c0c7ed99f4db
Lily <hanwen98@gmail.com>
2014-04-03 20:00:12 -0400

    fixed a dice rolling bug.

--- dice.py
+++ dice.py
@@ -1,6 +1,11 @@
 import random
 import specialInputModule
-dice = random.randint(1,6)
+
 side = specialInputModule.int_input("How many sides should the die have?")
 dice_number = specialInputModule.int_input("How many dice do you want to roll?")
-print "The result is %d" %(dice)
+
+for time in range(1,dice_number+1):
+	dice = random.randint(0,side)
+	print "The result is %d" %(dice)
+
+# 
