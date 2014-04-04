6926e38a35891f99cf5cea8fd7b29e18db1c7f42
Lily <hanwen98@gmail.com>
2014-04-03 20:18:15 -0400

    fixed the one side and negative number dice bug.

--- dice.py
+++ dice.py
@@ -1,11 +1,19 @@
+#! /usr/bin/env python
 import random
 import specialInputModule
 
 side = specialInputModule.int_input("How many sides should the die have?")
+while side <= 1:
+	print "That number is not valid."
+	side = specialInputModule.int_input("How many sides should the die have?")
+
 dice_number = specialInputModule.int_input("How many dice do you want to roll?")
+while dice_number <= 0:
+	print "That number is not valid."
+	dice_number = specialInputModule.int_input("How many dice do you want to roll?")
 
 for time in range(1,dice_number+1):
-	dice = random.randint(0,side)
+	dice = random.randint(1,side)
 	print "The result is %d" %(dice)
 
 # 
